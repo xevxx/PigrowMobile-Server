683a31d445a4a9c96e47d0b3c40e91ca919dd43f
xevxx <david.mcquillan@cianad.com>
2020-11-02 23:41:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ import os
 import csv
 import subprocess
 import json
+import datetime
+from dateutil import parser
 
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = False
 app.clients = {}
-app.config["HOME_PATH"] = r'/home/david/Pigrow' 
-home_path  = r'/home/david/Pigrow' 
+
+app.config["HOME_PATH"] = r'/home/pi/Pigrow' 
+home_path  = r'/home/pi/Pigrow' 
 # default return 
 @app.route('/', methods=['GET'])
 def home():
@@ -23,18 +26,19 @@ def RunSubprocess(args):
     process = subprocess.run(args, 
                          stdout=subprocess.PIPE, 
                          universal_newlines=True)
-    return process;
+    return process
 
 # A route to return all of the configured triggers.
-@app.route('/api/v1/triggers/getalltriggers', methods=['GET'])
-def api_GetCurrentTriggers():
+@app.route('/api/v1/triggers/getalltriggers/<local>', methods=['GET'])
+@app.route('/api/v1/triggers/getalltriggers', defaults={'local': None}, methods=['GET'])
+def api_GetCurrentTriggers(local):
 
     results = []
     with open(os.path.join(home_path, 'config/trigger_events.txt')) as csvfile:
         reader = csv.reader(csvfile)
         for row in reader: # each row is a list
             results.append(row)
-    
+
     triggers = []
     for line in results:
         trigg = {}
@@ -47,9 +51,13 @@ def api_GetCurrentTriggers():
         trigg["lock"] = line[6]
         trigg["cmd"] = line[7]
         triggers.append(trigg)
-    
-    return jsonify(triggers)
+    if local:
+        return triggers
+    else:
+        return jsonify(triggers)
 
+
+# get spcific trigger by condition name
 @app.route('/api/v1/triggers/gettrigger/<conditionname>', methods=['GET'])
 def api_GetTrigger(conditionname):
 
@@ -75,47 +83,59 @@ def api_GetTrigger(conditionname):
     
     return jsonify(trigg)
 
+# Set trigger value, update exisiting or create new
+# resultsJson['createnew'] decide which
 @app.route('/api/v1/triggers/settrigger', methods=['POST'])
 def api_SetTrigger():
-
+    resultsJson = request.get_json()
+    log = resultsJson['log']
+    valueLabel = resultsJson['valuelabel']
+    typeVal = resultsJson['type']
+    value = resultsJson['value']
+    conditionname = resultsJson['conditionname']
+    setVal = resultsJson['set']
+    lock = resultsJson['lock']
+    cmd = resultsJson['cmd']
+    createNew = resultsJson['createnew']
+    success = True
     results = []
-    if request.method == 'POST':
-        log = request.form['log']
-        valueLabel = request.form['valueLabel']
-        typeVal = request.form['type']
-        value = request.form['value']
-        conditionname = request.form['conditionname']
-        setVal = request.form['setVal']
-        lock = request.form['lock']
-        cmd = request.form['cmd']
-    with open(os.path.join(home_path, 'config/trigger_events.txt')) as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader: 
-            results.append(row)
-    with open(os.path.join(home_path, 'config/trigger_events.txt'), 'w') as outf:
-        writer = csv.writer(outf)
-    
-    try:
-        newline = True
-        for line in results:
-            if line[4] == conditionname:
-                writer.writerow(log,valueLabel,type,value,conditionname,setVal,lock,cmd)
-                newline = False
-                break
-            else:
-                writer.writerow(line)
-        writer.writerows(reader)
-        if newline == True:
-            writer.writerow(log,valueLabel,type,value,conditionname,setVal,lock,cmd)
-        status_code = flask.Response(status=201)
-    except:
-        raise InvalidUsage('Write failed', status_code=410)
-    
-    return status_code
+    if createNew:
+        try:
+            with open(os.path.join(home_path, 'config/trigger_events.txt'), 'a+') as file_object:
+                file_object.seek(0)
+                data = file_object.read(100)
+                if len(data) > 0 :
+                    file_object.write(",".join(map(str, [log,valueLabel,typeVal,value,conditionname,setVal,lock,cmd])) + '\n')
+        except (Exception) as e:
+            success = false
+    else:
+        with open(os.path.join(home_path, 'config/trigger_events.txt')) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader: 
+                results.append(row)
+        with open(os.path.join(home_path, 'config/trigger_events.txt'), 'w') as outf:
+            try:
+                for line in results:
+                    if len(line) > 0:
+                        if line[4] == conditionname:
+                            outf.write(",".join(map(str, [log,valueLabel,typeVal,value,conditionname,setVal,lock,cmd]))+ '\n')
+                        else:
+                            outf.write(",".join(map(str, line)) + '\n')
+                outf.close()
+            except (Exception) as e:
+                success = False
+                for line in results:
+                    outf.write(",".join(map(str, line)) + '\n')
+                outf.close()
+                #raise InvalidUsage('Write failed', status_code=410)
 
-# A route to get specific book details per user
-@app.route('/api/v1/config/getallsensors', methods=['GET'])
-def api_GetCurrentSensors():
+    return jsonify(str(success))
+
+
+# Get sensor details
+@app.route('/api/v1/config/getallsensors/<local>',  methods=['GET'])
+@app.route('/api/v1/config/getallsensors',defaults={'local': None}, methods=['GET'])
+def api_GetCurrentSensors(local):
     sensorsFull = {}
     with open(os.path.join(home_path, 'config/pigrow_config.txt')) as f:
         lines = f.read().splitlines()
@@ -127,10 +147,42 @@ def api_GetCurrentSensors():
                 else:
                     sensorsFull[linetuple[1]][linetuple[2].split('=')[0]] =  line.split('=')[1]
 
+    if local:
+        return sensorsFull
+    else:
+        return jsonify(sensorsFull)
 
-    return jsonify(sensorsFull)
 
-# A route to get specific book details per user
+# Get specific sensor details by name
+@app.route('/api/v1/config/readsensor/<sensorname>/<typeSensor>', methods=['GET'])
+@app.route('/api/v1/config/readsensor/<sensorname>', defaults={'typeSensor': 'modular'}, methods=['GET'])
+def api_ReadSensor(sensorname,typeSensor='modular'):
+    sensors = api_GetCurrentSensors(True)
+    sensorSplit = '\n'
+    if sensorname in sensors:
+        sensor = sensors[sensorname]
+        module_path = [home_path + "/scripts/gui/sensor_modules/sensor_" + sensor['type'] + ".py", "location=" + sensor['loc'] ,"name=" + sensorname]
+    if typeSensor.lower() == 'chirp':
+        module_path = [home_path + "/scripts/sensors/log_" + sensor['type'] + ".py", "address=" + sensor['loc'].split(":")[1]]
+        sensorSplit = '>'
+        if sensor["extra"]:
+            minmax = sensor['extra'].split(" ")
+            min = minmax[0].split(":")[1]
+            max = minmax[1].split(":")[1]
+            module_path.append("min=" + min)
+            module_path.append("max=" + max )
+
+    reading = RunSubprocess(module_path)
+    line = reading.stdout.strip()
+    line = line.replace('Written; ' ,'')
+    obj,error = ParseReading(line, typeSensor, None, sensorSplit)
+    obj["sensortype"] = sensorname
+    #objNamed = {sensorname: obj}
+        
+    return jsonify(obj)
+
+
+# Get and parse config file
 @app.route('/api/v1/config/getconfig', methods=['GET'])
 def api_GetConfig():
     sensorsFull = {}
@@ -152,11 +204,11 @@ def api_GetConfig():
                         config[linetuple[0]] = {linetuple[1].split('=')[0]:line.split('=')[1]}
                     else:
                         config[linetuple[0]][linetuple[1].split('=')[0]] =  line.split('=')[1]
-    config['sensors'] = sensorsFull;
+    config['sensors'] = sensorsFull
 
     return jsonify(config)
 
-# A route to get specific book details per user
+# Get gpio details
 @app.route('/api/v1/config/getgpio', methods=['GET'])
 def api_GetGpio():
     gpioFull = {}
@@ -173,21 +225,27 @@ def api_GetGpio():
 
     return jsonify(gpioFull)
 
-@app.route('/api/v1/config/setgpio', methods=['POST'])
-def api_SetGpio(relayName,direction):
+# set gpio toggle
+@app.route('/api/v1/config/setgpio/', methods=['POST'])
+def api_SetGpio():
+    data = request.get_json()
+    relayName = data['relayName']
+    direction = data['direction']
+    pin = data['pin']
+    pin = int(pin)
+    state = data['state']
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    gpios = json.load(api_GetGpio())
-    gp = gpios["gpio"]
-    gpio = gp[relayName]
-    gpioPowerState = gp[relayName + '_on']
-    args = ['echo', str(gpio) + '>' + '/sys/class/gpio/export']
+    gpioPowerState = direction
+    args = ['echo', str(pin) + '>' + '/sys/class/gpio/export']
     out = RunSubprocess(args)
-    args = ['cat', '/sys/class/gpio/gpio' + str(gpio) + '/value']
+    args = ['cat', '/sys/class/gpio/gpio' + str(pin) + '/value']
     out = RunSubprocess(args)
-    gpio_status = out.strip()
-    gpio_err = out.strip()
+    gpio_status = out.stdout.strip()
+    gpio_err = ''
+    if out.stderr is not None:
+        gpio_err = out.stderr.strip()
     success = True
     if gpio_status == "1":
         if gpioPowerState == 'low':
@@ -208,26 +266,68 @@ def api_SetGpio(relayName,direction):
     else:
         device_status = "read error -" + gpio_status + "-"
     if success:
-        GPIO.setup(gpio, GPIO.OUT)    
+        GPIO.setup(pin, GPIO.OUT)    
         if device_status == "OFF" or direction.upper() == "OFF":
             if gpioPowerState == "low":
-                GPIO.output(gpio, GPIO.LOW)
+                GPIO.output(pin, GPIO.LOW)
             elif gpioPowerState == "high":
-                GPIO.output(gpio, GPIO.HIGH)
+                GPIO.output(pin, GPIO.HIGH)
         elif device_status == "ON" or direction.upper() == "ON":
             if gpioPowerState == "low":
-                GPIO.output(gpio, GPIO.HIGH)
+                GPIO.output(pin, GPIO.HIGH)
             elif gpioPowerState == "high":
-                GPIO.output(gpio, GPIO.LOW)
+                GPIO.output(pin, GPIO.LOW)
         
     return success
 
+# Check gpio state
+@app.route('/api/v1/config/checkgpio/', methods=['POST'])
+def api_CheckGpio():
+    gpios = request.get_json()
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for gp in gpios:
+        if 'direction' in gp:
+            gpio = gp['pin']
+            gpioPowerState = gp['direction']
+            args = ['echo', str(gpio) + '>' + '/sys/class/gpio/export']
+            out = RunSubprocess(args)
+            args = ['cat', '/sys/class/gpio/gpio' + str(gpio) + '/value']
+            out = RunSubprocess(args)
+            gpio_status = out.stdout.strip()
+            gpio_err = ''
+            if out.stderr is not None:
+                gpio_err = out.stderr.strip()
+            success = True
+            if gpio_status == "1":
+                if gpioPowerState == 'low':
+                    device_status = "OFF"
+                elif gpioPowerState == 'high':
+                    device_status = 'ON'
+                else:
+                    device_status = "settings error"
+                    success = False
+            elif gpio_status == '0':
+                if gpioPowerState == 'low':
+                    device_status = "ON"
+                elif gpioPowerState == 'high':
+                    device_status = 'OFF'
+                else:
+                    device_status = "setting error"
+                    success = False
+            else:
+                device_status = "read error -" + gpio_status + "-"
+            gp['state'] = device_status
+            
+    return jsonify(gpios)
+
+# get log by logname
 @app.route('/api/v1/data/getlog/<logname>', defaults={'logtype': None}, methods=['GET'])
 @app.route('/api/v1/data/getlog/<logname>/<logtype>', methods=['GET'])
 def api_GetLog(logname,logtype='modular'):
     
-    sensorsText = api_GetCurrentSensors()
-    sensors = json.loads(sensorsText.data)
+    sensorsText = api_GetCurrentSensors(True)
 
     if logname.lower() in sensors:
         sensor = sensors[logname]
@@ -239,49 +339,138 @@ def api_GetLog(logname,logtype='modular'):
     return jsonify(logResults)
 
 
-def ParseLog(logPath, type):
+# get log by name and specififc fields
+# post json object contaings contents of logConfig below
+@app.route('/api/v1/data/getcustomlog/', methods=['POST'])
+def api_GetCustomLog():
+    logConfig = request.get_json()
+    logPath = logConfig['log']
+    logName = logConfig['name']
+    logColumn = logConfig['col']
+    logType = logConfig['type']
+    options = {}
+    options['logPath'] = logPath
+    options['logName'] = logName
+    options['logColumn'] = logColumn
+    options['logType'] = logType
+    
+    if 'datestart' in logConfig:
+        startDate = logConfig['datestart']
+        options['datestart'] = startDate
+    if 'dateend' in logConfig:
+        endDate = logConfig['dateend']
+        options['dateend'] = endDate
+
+    logResults,error = ParseLog(logPath,logType,options)
+    if len(logResults) == 0:
+        logResults.append({'error':error})
+    return jsonify(logResults)
+
+
+# Parse log
+def ParseLog(logPath, typeSensor, options=None):
     
     logsResults = []
     with open(logPath) as f:
         lines = f.read().splitlines()
-        error = "";
+        error = ""
         for line in lines:
-            linePart = line.split('>');
-            obj = {}
-            if type is not None and type.upper() == 'CHIRP':
-                try:
-                    obj['time'] = linePart[0]
-                except IndexError:
-                    error = 'noIndex'
-                try:
-                    obj['moisture'] = linePart[1]
-                except IndexError:
-                    error = 'noIndex'
-                try:
-                    obj['moistureperc'] = linePart[2]
-                except IndexError:
-                    error = 'noIndex'
-                try:
-                    obj['temperature'] = linePart[3]
-                except IndexError:
-                    error = 'noIndex'
-                try:
-                    obj['light'] = linePart[4]
-                except IndexError:
-                    error = 'noIndex'
-            else:
-                for part in linePart:
-                    try:
-                        split = part.split('=')
-                        obj[split[0]] = split[1]
-                    except:
-                        error = 'incorrect type maybe, default is modular'
-            
+            obj,error = ParseReading(line, typeSensor, options)
             if obj != {}:
                 logsResults.append(obj)
     
     return logsResults,error
 
+# parse log line (supports modular and chirp)
+def ParseReading(line, typeSensor, options=None, lineSplit = '>'):
+    linePart = line.split(lineSplit)
+    obj = {}
+    error = ''
+    if typeSensor is not None and typeSensor.upper() == 'CHIRP':
+        if options:
+            dt = linePart[0]
+            if 'datestart' in options:
+                start = datetime.datetime.strptime(options['datestart'], "%a, %d %b %Y %H:%M:%S %Z")
+                end = datetime.datetime.strptime(options['dateend'], "%a, %d %b %Y %H:%M:%S %Z")
+                lineDate = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S.%f')
+                if lineDate < start or lineDate > end:
+                    return obj,error
+            try:
+                obj['time'] = dt
+            except IndexError:
+                error = 'noIndex'
+            if 'moisture' in options['logColumn'] or not options['logColumn']:
+                try:
+                    obj['moisture'] = linePart[1]
+                except IndexError:
+                    error = 'noIndex'
+            if 'moistureperc' in options['logColumn'] or not options['logColumn']:
+                try:
+                    obj['moistureperc'] = linePart[2]
+                except IndexError:
+                    error = 'noIndex'
+            if 'temperature' in options['logColumn'] or not options['logColumn']:
+                try:
+                    obj['temperature'] = linePart[3]
+                except IndexError:
+                    error = 'noIndex'
+            if 'light' in options['logColumn'] or not options['logColumn']:
+                try:
+                    obj['light'] = linePart[4]
+                except IndexError:
+                    error = 'noIndex'
+        else:
+            try:
+                obj['time'] = linePart[0]
+            except IndexError:
+                error = 'noIndex'
+            try:
+                obj['moisture'] = linePart[1]
+            except IndexError:
+                error = 'noIndex'
+            try:
+                obj['moistureperc'] = linePart[2]
+            except IndexError:
+                error = 'noIndex'
+            try:
+                obj['temperature'] = linePart[3]
+            except IndexError:
+                error = 'noIndex'
+            try:
+                obj['light'] = linePart[4]
+            except IndexError:
+                error = 'noIndex'
+    elif not options:
+        for part in linePart:
+            try:
+                split = part.split('=')
+                obj[split[0]] = split[1]
+            except:
+                error = 'incorrect type maybe, default is modular'
+    else:
+        for part in linePart:
+            try:
+                split = part.split('=')
+                if 'datestart' in options:
+                    if split[0] == 'time':
+                        start = datetime.datetime.strptime(options['datestart'], "%a, %d %b %Y %H:%M:%S %Z")
+                        end = datetime.datetime.strptime(options['dateend'], "%a, %d %b %Y %H:%M:%S %Z")
+                        lineDate = datetime.datetime.strptime(split[1], '%Y-%m-%d %H:%M:%S.%f')
+                        if lineDate < start or lineDate > end:
+                            return obj,error
+                        else:
+                            obj[split[0]] = split[1]    
+                    if split[0] in options['logColumn'] or not options['logColumn']:
+                        obj[split[0]] = split[1]
+                else:
+                    if split[0] in options['logColumn'] or options['logColumn'] == {} or split[0] == 'time':
+                        obj[split[0]] = split[1]
+                
+                
+            except:
+                error = 'incorrect type maybe, default is modular'
+    
+    return obj,error
 
 class InvalidUsage(Exception):
     status_code = 400
@@ -304,7 +493,7 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-
+# avail able on local lan only
 app.run(host= '0.0.0.0')
 
 
